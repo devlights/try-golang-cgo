aad9a3158cc27f7df196f9d82cdc9602a94a007b
devlights <gsf.zero1@devlights.onmicrosoft.com>
2025-11-26 18:32:18 +0900

    Add 35.C_define_constants

---
+++ 35.C_define_constants/main.go
@@ -0,0 +1,33 @@
+package main
+
+/*
+#include <stdio.h>
+#include <stdlib.h>
+
+#define VALUE1    ((unsigned long)999)
+#define MACRO1(x) (printf("[C] %d\n", x))
+
+static inline void macro1(int x) {
+	MACRO1(x);
+}
+*/
+import "C"
+import "fmt"
+
+func main() {
+	//
+	// cgoでは
+	//   - defineで定義した定数は見える
+	//   - マクロは見えない
+	//
+	var (
+		v = C.ulong(C.VALUE1)
+	)
+	fmt.Printf("v=%d\n", v)
+
+	// これは無理。マクロは見えない。
+	// C.MACRO1(100)
+
+	// C側でラッパー関数用意して呼び出す
+	C.macro1(C.int(888))
+}
